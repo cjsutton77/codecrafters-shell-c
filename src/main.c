f388e26d8c0c314cb6a32cb0a84f82879041e0e3
Christian Sutton <christian.sutton@gmail.com>
2025-03-14 18:15:18 -0500

    loop

--- src/main.c
+++ src/main.c
@@ -3,16 +3,18 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+  while(true){
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-   printf("$ ");
+    // Uncomment this block to pass the first stage
+    printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = 0;
-  printf("%s: command not found\n", input);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    input[strcspn(input, "\n")] = 0;
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
